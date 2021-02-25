95d440aeb70b2da4014eb53c9e6d8f8b74bd6995
salihsimseek@gmail.com <salihsimseek@gmail.com>
2021-02-25 14:59:26 +0300

    spofity login service added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 import './App.css';
 
+import Login from './components/Login';
+import Player from './components/Player'
+
+const spotify = new SpotifyWebApi();
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log('person',user)
+      })
+    }
+  }, []);
+
+  return <div className="app">{token ? <Player />: <Login />}</div>;
 }
 
 export default App;
